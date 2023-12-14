c220cf9ff421c4f6142bb095ce8a2b4231279d03
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-12-14 12:25:55 +0100

    Refactor DocumentDetails component to use array for fields

--- app/src/DocumentDetails.tsx
+++ app/src/DocumentDetails.tsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const exampleFields = {
-    "name": "Paul",
-    "age": 25,
-}
+const exampleFields = [
+    "name",
+    "age"
+]
 
 const DocumentDetails = () => {
     const [name, setName] = React.useState<string>("");
-    const [fields, setFields] = React.useState<{ [key: string]: any }>({}); // Adjusting the type
-    const { id } = useParams<{ id: string }>(); // Extracting id from URL
+    const [fields, setFields] = React.useState<string[]>([]);
+    const { id } = useParams<{ id: string }>();
 
     React.useEffect(() => {
-        if (id) {
-            fetch(`http://localhost:8081/documents/${id}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setName(data.name);
-                    setFields(data.fields);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                    setName("Mock document name");
-                    setFields(exampleFields);
-                });
-        }
+        fetch(`http://localhost:8081/documents/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setName(data.name);
+                setFields(data.fields);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+
+                setName("Mock document name");
+                setFields(exampleFields);
+            });
+
     }, [id]);
 
     return (
         <div>
             <h1>{name}</h1>
             <ul>
-                {Object.entries(fields).map(([fieldKey, fieldValue]) => (
-                    <li key={fieldKey}>
-                        {fieldKey}: <input type="text" value={fieldValue} onChange={() => {}} />
+                {fields.map((field) => (
+                    <li key={field}>
+                        {field}: <input type="text" value={""} onChange={() => { }} />
                     </li>
                 ))}
             </ul>
