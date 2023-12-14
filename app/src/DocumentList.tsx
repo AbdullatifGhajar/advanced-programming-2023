897347febf6f97e75385ed333411ead1dd05ac45
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-12-14 11:20:42 +0100

    Add API call to fetch document data

--- app/src/DocumentList.tsx
+++ app/src/DocumentList.tsx
@@ -12,11 +12,25 @@ const dataFromApi: { [key: string]: { fields: string[] } } = {
 
 
 const DocumentList = () => {
+    const [documentData, setDocumentData] = React.useState<{ [key: string]: { fields: string[] } }>({});
+    
+    React.useEffect(() => {
+        fetch('localhost:3000/api/documents')
+        .then((response) => response.json())
+        .then((data) => {
+            setDocumentData(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            setDocumentData(dataFromApi);
+        });
+    }, []);
+    
     return (
         <div>
             <h1>Document List</h1>
             <ul>
-                {Object.keys(dataFromApi).map((documentId) => (
+                {Object.keys(documentData).map((documentId) => (
                     <li key={documentId}>
                         <DocumentForm documentId={documentId} fields={dataFromApi[documentId].fields} />
                     </li>
