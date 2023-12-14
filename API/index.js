e00950157142745e5d5703b0cb69056c16e922b9
Elvin Aupiais--Berthy <elvin.aupiais-berthy@efrei.net>
2023-12-14 13:50:31 +0100

    [ADD] add cors to allow all CORS requests

--- API/index.js
+++ API/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const documentRoutes = require("./documents/src/routes/documents")
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/documents', documentRoutes);
 
 
