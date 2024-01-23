753ea80a26a5095ca30b0125901d273cd9efdc55
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2024-01-23 20:14:56 +0100

    Add error messages for empty fields

--- app/src/pages/documents/DocumentDetailsPage.tsx
+++ app/src/pages/documents/DocumentDetailsPage.tsx
@@ -15,26 +15,22 @@ import ArrowBack from '@mui/icons-material/ArrowBack';
 import TextFieldItem from './fields/TextFieldItem';
 import CheckboxFieldItem from './fields/CheckboxFieldItem';
 import FileFieldItem from './fields/FileFieldItem';
+import FieldError from './fields/FieldError';
 import DocumentService from '../../services/DocumentService';
 
 import MainLayout from '../../layouts/MainLayout';
 import CenteredElement from '../../components/CenteredElement';
+import React from 'react';
 
 const DocumentDetailsPage = () => {
   const navigate = useNavigate();
 
   const { id: documentId } = useParams<{ id: string }>();
   const [fields, setFields] = useState<AnyField[]>([]);
-  const [fieldErrors, setFieldErrors] = useState<{ [key: string]: boolean }>(
-    {},
-  );
+  const [fieldErrors, setFieldErrors] = useState<FieldError>({});
+  const hasError = Object.values(fieldErrors).some((value) => value.length > 0);
 
   const documentService = new DocumentService();
-
-  const hasError = Object.values(fieldErrors).some((error) => error);
-  // const hasEmptyOrNullUndefinedValues = fields.some(
-  //   (field) => field.value == null || field.value.trim() === '',
-  // );
 
   // ensure that the documentId is defined
   useEffect(() => {
@@ -61,24 +57,37 @@ const DocumentDetailsPage = () => {
   // documentId is now defined for the rest of the component
   if (!documentId) return null;
 
+  const getFieldItem = (field: AnyField) => {
+    if (field.type === 'text') {
+      return (
+        <TextFieldItem
+          textField={field as TextField}
+          setFields={setFields}
+          setFieldErrors={setFieldErrors}
+        />
+      );
+    } else if (field.type === 'checkbox') {
+      return (
+        <CheckboxFieldItem
+          checkboxField={field as CheckboxField}
+          setFields={setFields}
+        />
+      );
+    } else if (field.type === 'file') {
+      return (
+        <FileFieldItem fileField={field as FileField} setFields={setFields} />
+      );
+    }
+    return null;
+  };
+
   const handleSave = async (event: React.FormEvent) => {
     event.preventDefault();
-    // // Check for null or undefined values
-    // const hasEmptyOrNullUndefinedValues = fields.some(
-    //   (field) => field.value == null || field.value.trim() === '',
-    // );
-    // if (hasEmptyOrNullUndefinedValues) {
-    //   setFieldErrors((prevErrors) => {
-    //     //add the errors in the array of errors
-    //     const updatedErrors: { [key: string]: boolean } = {};
-    //     fields.forEach((field) => {
-    //       if (field.value == null || field.value.trim() === '') {
-    //         updatedErrors[field.id] = true;
-    //       }
-    //     });
-    //     return updatedErrors;
-    //   });
-    // }
+
+    if (hasError) {
+      alert('Please fix all errors before saving');
+      return;
+    }
 
     documentService
       .saveFields(documentId, fields)
@@ -105,31 +114,12 @@ const DocumentDetailsPage = () => {
         </Box>
 
         <form onSubmit={handleSave}>
-          {fields.map((field) => {
-            if (field.type === 'text') {
-              return (
-                <TextFieldItem
-                  textField={field as TextField}
-                  setFields={setFields}
-                />
-              );
-            } else if (field.type === 'checkbox') {
-              return (
-                <CheckboxFieldItem
-                  checkboxField={field as CheckboxField}
-                  setFields={setFields}
-                />
-              );
-            } else if (field.type === 'file') {
-              return (
-                <FileFieldItem
-                  fileField={field as FileField}
-                  setFields={setFields}
-                />
-              );
-            }
-            return null;
-          })}
+          {fields.map((field) => (
+            <React.Fragment key={field.id}>
+              {getFieldItem(field)}
+              {fieldErrors[field.id] && <div style={{ color: 'red' }}>{fieldErrors[field.id]}</div>}
+            </React.Fragment>
+          ))}
 
           <CenteredElement>
             <Button
--- app/src/pages/documents/fields/TextFieldItem.tsx
+++ app/src/pages/documents/fields/TextFieldItem.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import { TextField as TextFieldComponent } from '@mui/material';
 
 import { AnyField, TextField } from '../../../models/Field';
+import FieldError from './FieldError';
 
 type TextFieldProps = {
   textField: TextField;
   setFields: React.Dispatch<React.SetStateAction<AnyField[]>>;
+  setFieldErrors: React.Dispatch<React.SetStateAction<FieldError>>;
 };
 
-const TextFieldItem: React.FC<TextFieldProps> = ({ textField, setFields }) => {
+const TextFieldItem: React.FC<TextFieldProps> = ({
+  textField,
+  setFields,
+  setFieldErrors,
+}) => {
+  const validate = (value: string) => {
+    let errorMessages: string[] = [];
+    if (value === '') errorMessages.push('This field is required.');
+
+    return errorMessages;
+  };
+
   const handleFieldChange = (newValue: string) => {
+    setFieldErrors((prevErrors) => {
+      const updatedErrors = { ...prevErrors };
+
+      updatedErrors[textField.id] = validate(newValue);
+      return updatedErrors;
+    });
+
     setFields((prevFields) => {
       const updatedFields = prevFields.map((field) => {
         if (field.id === textField.id) {
@@ -21,6 +41,7 @@ const TextFieldItem: React.FC<TextFieldProps> = ({ textField, setFields }) => {
       return updatedFields;
     });
   };
+
   return (
     <TextFieldComponent
       key={textField.id}
