432f218fb6d1cb0c6182732d0b215c1cb00484ed
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2024-01-25 16:01:09 +0100

    Use wait-for-it to wait for the database to finish before running the backend

--- API/start-api.sh
+++ API/start-api.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sleep 5
+
+# wait for the database to be ready
+./wait-for-it.sh -h $DB_HOST -p $DB_PORT -t 15
 
 # Create tables on the database
 npm run create-migration
 npm run migrate
 
-# Check if DEBUG then add data to the tables
+# if DEBUG mode then add data to the tables
 if [ "$DEBUG" = "true" ]; then
     npm run clear-data
     npm run add-data
